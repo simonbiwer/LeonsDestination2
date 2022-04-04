c9201a2cbff87b6772f4f70985e1d3588eba84d0
simonbiwer <91076673+simonbiwer@users.noreply.github.com>
2022-04-04 17:17:59 +0200

    Finally

--- app/src/main/java/com/example/leonsdestination2/MainActivity.java
+++ app/src/main/java/com/example/leonsdestination2/MainActivity.java
@@ -4,18 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity{
 
-    int LOCATION_REFRESH_TIME = 50; // 50  milliseconds to update
-    int LOCATION_REFRESH_DISTANCE = 1; // 1 meter to update
+    int LOCATION_REFRESH_TIME = 10; // 10  milliseconds to update
+    int LOCATION_REFRESH_DISTANCE = 0; // 0 meter to update
     int MY_PERMISSIONS_REQUEST_LOCATION = 99;
 
     private double latM = 50.7385400928;
@@ -23,36 +27,62 @@ public class MainActivity extends AppCompatActivity{
 
     private String perm1 = "android.permission.ACCESS_COARSE_LOCATION";
     private String perm2 = "android.permission.ACCESS_FINE_LOCATION";
+    private String perm3 = "android.permission.ACCESS_BACKGROUND_LOCATION";
 
     String[] perms = {perm1, perm2};
 
     private TextView myTextView;
     private ImageView myImageView;
+    private TextView counterView;
 
-    private final LocationListener mLocationListener = new LocationListener() {
-        @Override
-        public void onLocationChanged(final Location location) {
-            tick();
-        }
-    };
+    int tickCounter = -1;
+
+    private LocationManager mLocationManager;
+    private Location musiktruhe;
+
+    private LocationListener mLocationListener = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(final Location location) {
+//                myTextView.setText(location.toString());
+                tick(location);
+            }
+        };
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    protected void onStart() {
-        super.onStart();
         myTextView = (TextView)findViewById(R.id.mytextfield);
         myImageView = (ImageView) findViewById(R.id.imageViewArrow);
-        tick();
+        counterView = (TextView) findViewById(R.id.textViewCounter);
+        init();
+        tick(null);
+    }
+
+    private void init(){
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        musiktruhe = createMusiktruheAsLocation();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            ActivityCompat.requestPermissions(this, perms, MY_PERMISSIONS_REQUEST_LOCATION);
+
+
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+        }
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME, LOCATION_REFRESH_DISTANCE, mLocationListener);
+        mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, LOCATION_REFRESH_TIME, LOCATION_REFRESH_DISTANCE, mLocationListener);
     }
 
     public Location getLocation() {
         Location location = null;
         try {
-            LocationManager mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
             // getting GPS status
             boolean isGPSEnabled = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
 
@@ -60,7 +90,6 @@ public class MainActivity extends AppCompatActivity{
                 // no network provider is enabled
 
             } else {
-                // First get location from Network Provider
                 //get the location by gps
                 if (isGPSEnabled) {
                     if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -75,7 +104,7 @@ public class MainActivity extends AppCompatActivity{
                         // to handle the case where the user grants the permission. See the documentation
                         // for ActivityCompat#requestPermissions for more details.
                     }
-                    if (mLocationListener != null && LocationManager.GPS_PROVIDER != null){
+                    if (mLocationListener != null){
                         mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME, LOCATION_REFRESH_DISTANCE, mLocationListener);
                         if (mLocationManager != null) {
                             location = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
@@ -115,14 +144,16 @@ public class MainActivity extends AppCompatActivity{
         return m;
     }
 
-    private void tick(){
-        Location location = getLocation();
+    private void tick(Location location){
+        counterView.setText(String.valueOf(tickCounter++));
+//        Location location = getLocation();
         if (location == null) {
             myTextView.setText("Standort nicht abrufbar");
         } else {
             Double winkel = bearing(location.getLatitude(), location.getLongitude(), latM, lngM);
-            Float distance = location.distanceTo(createMusiktruheAsLocation());
+            Float distance = location.distanceTo(musiktruhe);
             myTextView.setText(String.format("Noch %.4g km bis zum Ziel", distance/1000));
+//            location.setBearing(90);
             Float actBearing = location.getBearing();
             myImageView.setRotation(winkel.floatValue() - 90 - actBearing);
         }
